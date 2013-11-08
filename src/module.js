d507187f3b2bff2c1e4eadfa3ae12ecddc590012
Andrew Rota <rota.andrew@gmail.com>
2013-11-07 23:42:15 -0500

    Add documentation comments

--- src/module.js
+++ src/module.js
@@ -1,15 +1,38 @@
 /**
- * Module design pattern
+ * A module design pattern
  *
  * @class module
+ * @return {Object} Returns the module API as an object
  */
 
 module.exports = (function() {
+  /**
+   * A private property
+   *
+   * @property _privateProperty
+   * @type {String}
+   * @default 'bar'
+   * @private
+   */
   var _privateProperty = 'bar';
   return {
+    /**
+     * A public method which returns this.publicProperty
+     * concatenated with _privateProperty
+     *
+     * @method publicMethod
+     * @type {Function}
+     */
     publicMethod: function() {
       return this.publicProperty + ' ' + _privateProperty;
     },
+    /**
+     * A public property'
+     *
+     * @property publicProperty
+     * @type {String}
+     * @default 'foo'
+     */
     publicProperty: 'foo'
   };
 }());
