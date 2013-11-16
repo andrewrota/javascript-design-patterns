cedf3ff730084d935db99c031c3848d156821616
Andrew Rota <rota.andrew@gmail.com>
2013-11-16 14:34:17 -0500

    Refactoring and renaming.  Adding some comments for docs.

--- src/singleton.js
+++ src/singleton.js
@@ -22,13 +22,34 @@ module.exports = (function() {
    * @return {Object} Returns the API for singleton functionality
    */
   var init = function() {
-    var value = Math.random();
+    /**
+     * Example of a property on the singleton API.  Uses
+     * Math.random() to generate a random number to prove
+     * whether the pattern is returning the same instance
+     *
+     * @property _value
+     * @type {Number}
+     * @private
+     */
+    var _value = Math.random();
     return {
-      randomNumber: value
+      /**
+       * Get a unique number for this pattern.
+       *
+       * @property uniqueNumber
+       * @type {Number}
+       */
+      uniqueNumber: _value
     };
   };
 
   return {
+    /**
+     * A method to get the instance of the singleton
+     *
+     * @method getInstance
+     * @return {Object} Returns the instance of the singleton
+     */
     getInstance: function() {
       if(!_instance) {
         _instance = init();
